7f17436450935280ffec583125bc86b8ec498c73
tumelo-maja <mogale.ramsey@gmail.com>
2025-06-01 13:14:14 +0100

    Add base and index templates, and static css file

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,12 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
+
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    #template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
+
+
+
