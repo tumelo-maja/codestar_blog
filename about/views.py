69acdb9fec0ab2fdd45b470ffd614bbbd439819f
tumelo-maja <mogale.ramsey@gmail.com>
2025-06-04 16:07:19 +0100

    Add form submission handling for collaboration requests

--- about/views.py
+++ about/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
@@ -20,22 +20,18 @@ def about_me(request):
     :template:`blog/about.html`
     """
 
-    collaborate_form = CollaborateForm()
-    # collaborate_form = CollaborateForm(data=request.POST)
-    # if collaborate_form.is_valid():
-    #     collaboration = collaborate_form.save(commit=False)
-    #     # comment.author = request.user
-    #     # comment.post = post
-    #     collaboration.save()
-    #     messages.add_message(
-    #         request, messages.SUCCESS,
-    #         'Collaboration request submitted successfully'
-    #     )
 
-    # queryset = About.objects.all()
-    # about = get_object_or_404(queryset)
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
 
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
